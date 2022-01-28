a0b12b966202791c8606659ee1bd6a25f0f6197d
abhishek <jeet45171@gmail.com>
2022-01-28 18:31:14 +0530

    first Commit By Akey_Singh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
+import { connect } from "react-redux";
+
+import Navbar from "./components/Navbar/Navbar";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
+import SingleItem from "./components/SingleItem/SingleItem";
+
+function App({ current }) {
+
+
+
+  useEffect(() => {
+    console.log(current);
+  },)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route  path="/" element={<Products/>} />
+          <Route  path="/cart" element={<Cart/>} />
+          {false ? ( <h1>Error Found</h1>) : (<Route  path="/product/:id" element={<SingleItem/>} />)}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    current: state.shop.currentItem,
+  };
+};
+
+export default connect(mapStateToProps)(App);
